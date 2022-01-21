776f4c17917d24ca856bb14cc6fec8c2cf9eba67
jefferson250598 <amarantegeraldelli@gmail.com>
2022-01-21 15:10:42 -0300

    primeiro commit

---
+++ scrpit.js
@@ -0,0 +1,28 @@
+// 1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(let i =1; i<= 10; i++){
+    console.log()
+ }
+
+
+
+// 2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let contador = 10
+while(1 <= contador){
+    console.log(contador)
+    contador +=2
+  }
+
+
+// 3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let conta = 1; conta <=100; conta +=2){
+    console.log(conta)
+  }
+
+// 4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(let conta = 0; conta <=100; conta +=2){
+    console.log(conta)
+}
